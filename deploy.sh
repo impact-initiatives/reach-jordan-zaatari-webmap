f7b1e21e66889b30f6b47e47223bfe6ba71636cc
Maxym Malynowsky <max@sparkgeo.com>
2016-09-20 19:49:17 +0300

    remove root from invalidation

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ aws s3 cp --recursive dist/ s3://$BUCKET/dist
 aws s3 cp --recursive node_modules/mapbox-gl/dist/ s3://$BUCKET/node_modules/mapbox-gl/dist
 aws configure set preview.cloudfront true
 aws cloudfront create-invalidation --distribution-id $AWS_CLOUDFRONT_DISTRIBUTION_ID \
-  --paths /*
+  --paths *

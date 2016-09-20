318ae5cf829d7593b0bc2b95f5a5c1cff05eab3b
Maxym Malynowsky <max@sparkgeo.com>
2016-09-20 11:31:07 +0300

    move common resources to separate repository

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,5 @@
 BUCKET=$1
 echo "Copy dist files to $BUCKET"
 aws s3 cp --recursive dist/ s3://$BUCKET/jordan-zaatari-health-map
+aws cloudfront get-invalidation --distribution-id $AWS_CLOUDFRONT_DISTRIBUTION_ID \
+  --paths /jordan-zaatari-health-map/*
--- src/constants/resources.js
+++ src/constants/resources.js
@@ -1,18 +1,20 @@
+const RESOURCES_URL = 'https://reach.maxmalynowsky.com/jordan-common';
+
 export const IMG = {
-  HOME_WHITE: '/resources/img/ic_home_white_24px.svg',
-  MENU_WHITE: '/resources/img/ic_menu_white_24px.svg',
-  LOGO_REACH: '/resources/img/logo-reach.png',
-  LOGO_UNHCR: '/resources/img/logo-unhcr.png',
+  HOME_WHITE: `${RESOURCES_URL}/img/ic_home_white_24px.svg`,
+  MENU_WHITE: `${RESOURCES_URL}/img/ic_menu_white_24px.svg`,
+  LOGO_REACH: `${RESOURCES_URL}/img/logo-reach.png`,
+  LOGO_UNHCR: `${RESOURCES_URL}/img/logo-unhcr.png`,
 };
 
 export const MAPBOX = {
   STYLE: {
-    EMPTY: '/resources/mapbox/styles/empty-v9.json',
+    EMPTY: `${RESOURCES_URL}/mapbox/styles/empty-v9.json`,
   },
 };
 
 export const REACH = {
-  DISTRICT_BOUNDARIES: '/resources/reach/district-boundaries.geojson',
-  BLOCK_BOUNDARIES: '/resources/reach/block-boundaries.geojson',
-  CAMP_FACILITIES: '/resources/reach/camp-facilities.geojson',
+  DISTRICT_BOUNDARIES: `${RESOURCES_URL}/reach/district-boundaries.geojson`,
+  BLOCK_BOUNDARIES: `${RESOURCES_URL}/reach/block-boundaries.geojson`,
+  CAMP_FACILITIES: `${RESOURCES_URL}/reach/camp-facilities.geojson`,
 };

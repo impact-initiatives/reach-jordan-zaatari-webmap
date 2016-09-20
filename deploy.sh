e76312da8cdec388327d575451b334fee1edf3a3
Maxym Malynowsky <max@sparkgeo.com>
2016-09-20 19:20:48 +0300

    add mapbox-gl to build

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ aws s3 cp index-dist.html s3://$BUCKET/index.html
 aws s3 cp service-worker.js s3://$BUCKET/service-worker.js
 aws s3 cp --recursive resources/ s3://$BUCKET/resources
 aws s3 cp --recursive dist/ s3://$BUCKET/dist
+aws s3 cp --recursive node_modules/mapbox-gl/dist/ s3://$BUCKET/node_modules/mapbox-gl/dist
 aws configure set preview.cloudfront true
 aws cloudfront create-invalidation --distribution-id $AWS_CLOUDFRONT_DISTRIBUTION_ID \
   --paths /*

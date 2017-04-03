199028a227aafe08385cbbada1a31b0b60b069cf
Max Malynowsky <maxmalynowsky@gmail.com>
2017-04-03 23:21:16 +0300

    update pipes data for septic tanks

--- src/mapbox-gl/sources/pipes.js
+++ src/mapbox-gl/sources/pipes.js
@@ -1,13 +1,21 @@
 import reach from '../../constants/reach.js';
 import utils from '../utils/index.js';
 import sources from '../../constants/sources.js';
+import keys from '../../constants/keys/pipes.js';
 
 function sourcePipes({ map }) {
   return fetch(reach.PIPES)
     .then((response) => response.json())
     .then(({ features }) => {
-      utils.addSourceToMap({ features, map, sourceId: sources.PIPES });
+      const newFeatures = features.map(modifyFeatures);
+      utils.addSourceToMap({ features: newFeatures, map, sourceId: sources.PIPES });
     });
+}
+
+function modifyFeatures(feature) {
+  const newFeature = feature;
+  newFeature.properties[keys.ID] = '';
+  return newFeature;
 }
 
 export default sourcePipes;

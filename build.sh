ae84e52f0cdccab30a0160a9dfe8b7bee0f08cb3
Max Malynowsky <maxmalynowsky@gmail.com>
2017-04-05 17:30:39 +0300

    remove unused dist files

--- build.sh
+++ build.sh
@@ -4,13 +4,15 @@ rm -rf ./dist/
 
 webpack -p
 
-mkdir -p ./dist/node_modules/mapbox-gl/
+mkdir -p ./dist/node_modules/mapbox-gl/svg/
 mkdir -p ./dist/node_modules/normalize.css/
 
 cp ./manifest.json ./dist/manifest.json
 cp -a ./resources/. ./dist/resources/
-cp -a ./node_modules/normalize.css/. ./dist/node_modules/normalize.css/
-cp -a ./node_modules/mapbox-gl/dist/. ./dist/node_modules/mapbox-gl/dist/
+cp -a ./node_modules/normalize.css/normalize.css ./dist/node_modules/normalize.css/normalize.css
+cp -a ./node_modules/mapbox-gl/dist/svg/. ./dist/node_modules/mapbox-gl/dist/svg/
+cp ./node_modules/mapbox-gl/dist/mapbox-gl.css ./dist/node_modules/mapbox-gl/dist/mapbox-gl.css
+cp ./node_modules/mapbox-gl/dist/mapbox-gl.js ./dist/node_modules/mapbox-gl/dist/mapbox-gl.js
 
 appcache-manifest-fixer ./dist/index.html \
   -m ./cache.manifest \
